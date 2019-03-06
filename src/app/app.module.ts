36b5cba5e92bde601e3c3b9cdf33a809ee943ffe
Ishan Lakshitha <ishlakshitha@gmail.com>
2019-03-06 12:13:18 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ManageCustomerComponent } from './manage-customer/manage-customer.component';
+import { ManageItemsComponent } from './manage-items/manage-items.component';
+import { PlaceOrderComponent } from './place-order/place-order.component';
+import { ViewOrderComponent } from './view-order/view-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ManageCustomerComponent,
+    ManageItemsComponent,
+    PlaceOrderComponent,
+    ViewOrderComponent
   ],
   imports: [
     BrowserModule,
